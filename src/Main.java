f5689ad7e9ebd8004068281bdb70474b1864f060
Anastasiia Zyrianova <nasttttek@gmail.com>
2021-11-01 15:15:33 +0300

    RC6 with CFB

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,92 @@
-import org.bouncycastle.crypto.engines.RC6Engine;
 import java.security.SecureRandom;
-import org.bouncycastle.crypto.modes.CFBBlockCipher;
-import org.bouncycastle.crypto.paddings.PKCS7Padding;
-import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
-import org.bouncycastle.crypto.params.KeyParameter;
-import org.bouncycastle.crypto.params.ParametersWithIV;
-import org.bouncycastle.util.encoders.Base64;
 
-import javax.crypto.Cipher;
-import java.io.*;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите ключ: ");
-        Scanner scanner = new Scanner(System.in);
-        String userKey = scanner.nextLine();
+        // Считывем ключ из файла.
+        String userKey = Reader.readText("src/key");
+        System.out.println("Key is: " + userKey);
 
+        // Считываем текст для кодирования.
         String textFromFile = Reader.readText("src/test");
         System.out.println("Original text: " + textFromFile);
-        byte[] textToEncode = RC6.hexStringToByteArray(textFromFile);
-        byte[] key = RC6.hexStringToByteArray(userKey);
+        // Дополняем строчку до кратности 16.
+        int remainder = textFromFile.length() % 16;
+        if (remainder != 0) {
+            for (int i = 0; i < 16 - remainder; i++) {
+                textFromFile = textFromFile.concat(" ");
+            }
+        }
+        // Превращаем текст и ключ в массив байт.
+        byte[] textToEncode = RC6.stringToByteArray(textFromFile);
+        byte[] key = RC6.stringToByteArray(userKey);
 
         System.out.println("Starting to encrypt.");
-        byte[] cipherText = RC6.encrypt(textToEncode,key);
-
-        String encryptedText = RC6.convertToHex(cipherText);
+        // Инициализируем массив для хранения зашифрованного текста.
+        byte[] cipherText = new byte[textToEncode.length];
+        // Отделяем первый кусок текста - 16 байт.
+        byte[] slice = Arrays.copyOfRange(textToEncode, 0,  16);
+        // Инициализируем вектор инициализации.
+        byte[] iv = (new SecureRandom()).generateSeed(16);
+        // Шифруем вектор инициализации.
+        byte[] encrypted = RC6.encrypt(iv, key);
+        // Проводим операцию исключающего или для зашифрованного вектора и первого куска текста.
+        byte[] cipherSlice = xor(encrypted, slice);
+        // Зашифрованный кусок текста добавляем к массиву для получения конечного результата шифрования.
+        System.arraycopy(cipherSlice, 0, cipherText, 0, 16);
+        // Делаем те же операции для остатка текста.
+        for (int i = 16; i < textToEncode.length; i += 16) {
+            // Выделяем следующий кусок текста.
+            slice = Arrays.copyOfRange(textToEncode, i, i + 16);
+            // Повторно шифруем зашифрованный кусок текста.
+            encrypted = RC6.encrypt(cipherSlice, key);
+            // Проводим операцию исключающего или для повторно зашифрованного куска и для незашифрованного куска текста.
+            cipherSlice = xor(encrypted, slice);
+            // Добавляем новый кусок текста к конечному массиву.
+            System.arraycopy(cipherSlice, 0, cipherText, i, 16);
+        }
+        // Конвертируем массив байт в строку.
+        String encryptedText = RC6.convertToString(cipherText);
         System.out.println("Encrypted: " + encryptedText);
 
 
         System.out.println("Starting to decrypt.");
-        byte[] decipherText = RC6.decrypt(cipherText,key);
-        String decryptedText = RC6.convertToHex(decipherText);
+        // Инициализируем массив байт для конечного дешифрованного текста.
+        byte[] decipherText = new byte[textToEncode.length];
+        // Выделяем первый кусок.
+        slice = Arrays.copyOfRange(cipherText, 0, 16);
+        // Шифруем вектор инициализации.
+        byte[] decrypted = RC6.encrypt(iv, key);
+        // Проводим операцию исключающего или для вектора и куска зашифрованного текста.
+        byte[] decipherSlice = xor(decrypted, slice);
+        // Копируем полученный кусок в конечный массив.
+        System.arraycopy(decipherSlice, 0, decipherText, 0, 16);
+        for (int i = 16; i < textToEncode.length; i += 16) {
+            // Сохраняем предыдущий зашифрованный кусок в новую переменную.
+            byte[] prevSlice = slice;
+            // Получаем следующий кусок текста.
+            slice = Arrays.copyOfRange(cipherText, i, i + 16);
+            // Повторно шифруем зашифрованный кусок текста.
+            decrypted = RC6.encrypt(prevSlice, key);
+            // Выполняем операцию исключающего или для повторно зашифрованного куска и единожды зашифрованного куска текста.
+            decipherSlice = xor(decrypted, slice);
+            // Копируем в конечный массив.
+            System.arraycopy(decipherSlice, 0, decipherText, i, 16);
+        }
+        // Конвертируем массив байт в строку.
+        String decryptedText = RC6.convertToString(decipherText);
         System.out.println("Decrypted: " + decryptedText);
+    }
 
 
+    private static byte[] xor(byte[] array_1, byte[] array_2) {
+        byte[] result = new byte[array_1.length];
+
+        int i = 0;
+        for (byte b : array_1) {
+            result[i] = (byte) (b ^ array_2[i++]);
+        }
+        return result;
     }
 }
--- src/RC6.java
+++ src/RC6.java
@@ -1,77 +1,63 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class RC6 {
-
-
-    // Константы для операций сдвига
+    // Константы для операций сдвига.
     private static int r = 20, w = 32;
     // Крипто-ключ
     private static int[] S = new int[r * 2 + 4];
 
-    //
+    // По формулам генерируются два псевдослучайных числа. Для w = 32 они принимают значения, представленные ниже.
     private static int Pw = 0xB7E15163;
     private static int Qw = 0x9E3779b9;
 
-    // Функция сдвига влево
-    private static int rotateL(int n, int x){
+    // Функция сдвига влево.
+    private static int shiftL(int n, int x) {
         return ((n << x) | (n >>> (w - x)));
     }
 
-    // Функция сдвига вправо
-    private static int rotateR(int n, int x){
-        return ((n >>> x) | (n << (w - x)));
-    }
-
-
-    public static byte[] hexStringToByteArray(String s) {
-        int string_len = s.length();
-        byte[] data = new byte[string_len / 2];
-        for (int i = 0; i < string_len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character
-                    .digit(s.charAt(i + 1), 16));
-        }
-        return data;
+    // Функция генерации массива байтов из строки.
+    public static byte[] stringToByteArray(String s) {
+        return s.getBytes(StandardCharsets.UTF_8);
     }
 
     // Генерация ключа
-    public static void keyGen(byte[] key){
+    public static void keyGen(byte[] key) {
+        // Разбиение ключа на машинные слова.
         int bytes = w / 8;
         int c = key.length / bytes;
         int[] L = new int[c];
         int index = 0;
 
-        for(int i = 0; i < c; i++){
+        for (int i = 0; i < c; i++) {
             L[i] = ((key[index++]) & 0xff | (key[index++] & 0xff) << 8 | (key[index++] & 0xff) << 16 | (key[index++] & 0xff) << 24);
         }
+
+        // Инициализация массива ключей.
         S[0] = Pw;
-
-        for(int i = 1; i <= 2*r+3; i++){
-            S[i] = S[i-1] + Qw;
+        for (int i = 1; i <= 2 * r + 3; i++) {
+            S[i] = S[i - 1] + Qw;
         }
+        // Перемешиваем секретный ключ пользователя.
+        int A = 0, B = 0, i = 0, j = 0;
+        int v = 3 * Math.max(c, 2 * r + 4);
 
-        int A = 0, B = 0, i = 0,j =0;
-        int v = 3 * Math.max(c, 2*r+4);
-
-        for(int k = 1;k <= v; k++){
-            A = S[i] = rotateL(S[i] + A + B, 3);
-            B = L[j] = rotateL(L[j] + A + B, A+B);
+        for (int k = 1; k <= v; k++) {
+            A = S[i] = shiftL(S[i] + A + B, 3);
+            B = L[j] = shiftL(L[j] + A + B, A + B);
             i = (i + 1) % (2 * r + 4);
             j = (j + 1) % c;
-        };
-    }
-
-    public static String convertToHex(byte[] text){
-        StringBuilder out = new StringBuilder();
-        for (int i = 0; i < text.length; i++) {
-            out.append(String.format("%02x ", text[i]));
         }
-        return out.toString();
+
+    }
+    // Функция генерации строки из массива байт.
+    public static String convertToString(byte[] text) {
+        return new String(text, StandardCharsets.UTF_8);
     }
 
-    public static byte[] allignHex(int regA,int regB, int regC, int regD){
+    // Объединяем блоки в массив байтов.
+    public static byte[] align(int regA, int regB, int regC, int regD) {
         int[] data = new int[4];
+
         byte[] text = new byte[w / 2];
 
         data[0] = regA;
@@ -79,32 +65,38 @@ public class RC6 {
         data[2] = regC;
         data[3] = regD;
 
-        for(int i = 0;i < text.length;i++){
-            text[i] = (byte)((data[i/4] >>> (i%4)*8) & 0xff);
+        for (int i = 0; i < text.length; i++) {
+            text[i] = (byte) ((data[i / 4] >>> (i % 4) * 8) & 0xff);
         }
         return text;
     }
-
-    public static byte[] encrypt(byte[] plainText, byte[] userKey){
+    // Функция шифрования.
+    public static byte[] encrypt(byte[] plainText, byte[] userKey) {
         int regA, regB, regC, regD;
+        int[] dataBlock= new int[plainText.length/4];
         int index = 0, temp1, temp2, swap;
 
-        regA = ((plainText[index++] & 0xff) | (plainText[index++] & 0xff) << 8| (plainText[index++] & 0xff) << 16| (plainText[index++] & 0xff)<<24);
-        regB = ((plainText[index++] & 0xff) | (plainText[index++] & 0xff) << 8| (plainText[index++] & 0xff) << 16| (plainText[index++] & 0xff)<<24);
-        regC = ((plainText[index++] & 0xff) | (plainText[index++] & 0xff) << 8| (plainText[index++] & 0xff) << 16| (plainText[index++] & 0xff)<<24);
-        regD = ((plainText[index++] & 0xff) | (plainText[index++] & 0xff) << 8| (plainText[index++] & 0xff) << 16| (plainText[index++] & 0xff)<<24);
-
+        // Формирование блоков.
+        for(int i=0;i<dataBlock.length;i++) {
+            dataBlock[i] =(plainText[index++] & 0xFF)| ((plainText[index++]& 0xFF)<<8) | ((plainText[index++]& 0xFF)<<16)|((plainText[index++]& 0xFF)<<24);
+        }
+        regA = dataBlock[0];
+        regB = dataBlock[1];
+        regC=  dataBlock[2];
+        regD = dataBlock[3];
+        // Генерация ключа для раунда шифрования.
         keyGen(userKey);
-
         regB = regB + S[0];
         regD = regD + S[1];
 
-        for(int i = 1; i <= r ; i++){
-            temp1 = rotateL(regB * (regB * 2 + 1), (int)(Math.log(w)/Math.log(2)));
-            temp2 = rotateL(regD * (regD * 2 + 1), (int)(Math.log(w)/Math.log(2)));
-            regA = (rotateL(regA ^ temp1, temp2)) + S[i * 2];
-            regC = (rotateL(regC ^ temp2, temp1)) + S[i * 2 + 1];
-
+        // Операция сдвига смешивает биты в слове.
+        for (int i = 1; i <= r; i++) {
+            temp1 = shiftL(regB * (regB * 2 + 1), (int) (Math.log(w) / Math.log(2)));
+            temp2 = shiftL(regD * (regD * 2 + 1), (int) (Math.log(w) / Math.log(2)));
+            //Используем xor в качестве нелинейного преобразования с хорошими показателями перемешивания битового значения входной величины.
+            regA = (shiftL(regA ^ temp1, temp2)) + S[i * 2];
+            regC = (shiftL(regC ^ temp2, temp1)) + S[i * 2 + 1];
+            // Меняем регистры местами.
             swap = regA;
             regA = regB;
             regB = regC;
@@ -115,40 +107,8 @@ public class RC6 {
         regA = regA + S[r * 2 + 2];
         regC = regC + S[r * 2 + 3];
 
-        return allignHex(regA, regB, regC, regD);
-    }
-
-    public static byte[] decrypt(byte[] cipherText, byte[]userKey){
-        int regA, regB, regC, regD;
-        int index = 0, temp1, temp2, swap;
-
-        regA = ((cipherText[index++] & 0xff) | (cipherText[index++] & 0xff) << 8| (cipherText[index++] & 0xff) << 16| (cipherText[index++] & 0xff)<<24);
-        regB = ((cipherText[index++] & 0xff) | (cipherText[index++] & 0xff) << 8| (cipherText[index++] & 0xff) << 16| (cipherText[index++] & 0xff)<<24);
-        regC = ((cipherText[index++] & 0xff) | (cipherText[index++] & 0xff) << 8| (cipherText[index++] & 0xff) << 16| (cipherText[index++] & 0xff)<<24);
-        regD = ((cipherText[index++] & 0xff) | (cipherText[index++] & 0xff) << 8| (cipherText[index++] & 0xff) << 16| (cipherText[index++] & 0xff)<<24);
-
-        keyGen(userKey);
-
-        regC = regC - S[r * 2 + 3];
-        regA = regA - S[r * 2 + 2];
-
-        for(int i = r; i >= 1 ; i--){
-            swap = regD;
-            regD = regC;
-            regC = regB;
-            regB = regA;
-            regA = swap;
-
-            temp2 = rotateL(regD * (regD * 2 + 1), (int)(Math.log(w)/Math.log(2)));
-            temp1 = rotateL(regB * (regB * 2 + 1), (int)(Math.log(w)/Math.log(2)));
-            regC =  rotateR(regC - S[i * 2 + 1], temp1) ^ temp2;
-            regA =  rotateR(regA -  + S[i * 2], temp2) ^ temp1;
-        }
-
-        regD = regD - S[1];
-        regB = regB - S[0];
-
-        return allignHex(regA, regB, regC, regD);
+        // Объединяем блоки.
+        return align(regA, regB, regC, regD);
     }
 
 }
